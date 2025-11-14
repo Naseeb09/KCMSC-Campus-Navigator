e38fb56772a4df7b7f0544a13382cca0f8ffff64
Sayu <igtjgtvt@gmail.com>
2025-11-14 15:39:33 +0600

    update floor-2

--- data.py
+++ data.py
@@ -1,24 +1,26 @@
 # data.py
-# KCMSC Tour Guide — Floor 1 Data
+# KCMSC Tour Guide — Floor 2 Data
 
 campus_data = {
-    "Floor 1": [
-        {"room": "201", "class": "One", "section": "Ka", "version": "English"},
-        {"room": "202", "class": "Nursery", "section": "Peacock & Bulbul"},
-        {"room": "203", "class": "Nursery", "section": "Myna & Parrot"},
-        {"room": "204", "class": "Nursery", "section": "Dove & Magpie"},
-        {"room": "205", "class": "Nursery", "section": "Swan & Sparrow"},
-        {"room": "206", "class": "N/A", "section": "Principal's Room"},
-        {"room": "207", "class": "KG", "section": "Marigold"},
-        {"room": "208", "class": "N/A", "section": "N/A"},
-        {"room": "209", "class": "KG", "section": "Carnation & Sunflower"},
-        {"room": "210", "class": "KG", "section": "Camillia & Bluebell"},
-        {"room": "211", "class": "One", "section": "Kha"},
-        {"room": "212", "class": "Dance Room", "section": "N/A"},
-        {"room": "213", "class": "N/A", "section": "N/A"},
+    "Floor 2": [
+        {"room": "301", "class": "One", "section": "A", "version": "English"},
+        {"room": "302", "class": "One", "section": "B", "version": "English"},
+        {"room": "303", "class": "One", "section": "C", "version": "English"},
+        {"room": "304", "class": "One", "section": "D", "version": "English"},
+        {"room": "305", "class": "Two", "section": "ka", "version": "Bangla"},
+        {"room": "306", "class": "Two", "section": "kha", "version": "Bangla"},
+        {"room": "307", "class": "Two", "section": "ga", "version": "Bangla"},
+        {"room": "308", "class": "Two", "section": "A", "version": "English"},
+        {"room": "309", "class": "N/A", "section": "Vice-Principal's Room"},
+        {"room": "310", "class": "Two", "section": "B", "version": "English"},
+        {"room": "311", "class": "Two", "section": "C", "version": "English"},
+        {"room": "312", "class": "N/A", "section": "Teacher's room"},
+        {"room": "313", "class": "N/A", "section": "English version Co-ordinator's Room (junior section)"},
     ],
     "Highlights": [
-        "⭐ Room 206 — Principal's Room",
-        "🏛️ Chairman’s & Vice-Chairman’s Room"
+        "⭐ Room 309 — Vice Principal's Room",
+        "⭐ Room 312 — Teacher's Room",
+        "⭐ Room 313 — English Version Co-ordinator's Room (junior section)",
+        "⭐ Teacher's common room"
     ]
 }
